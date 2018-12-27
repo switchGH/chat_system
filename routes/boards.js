ee1b4c8223615934634b4c828e8734e4cdce94a6
switch <shg18.sk1.hr10@gmail.com>
2018-12-27 15:31:56 +0900

    メッセージにユーザーを紐づけ

--- routes/boards.js
+++ routes/boards.js
@@ -6,7 +6,7 @@ const connection = require('../model/mysqlConnection');
 router.get('/:board_id', function(req, res, next){
   let boardId = req.params.board_id;
   let getBoardQuery = 'SELECT * FROM boards WHERE board_id = ' + boardId;
-  let getMessagesQuery = 'SELECT *, DATE_FORMAT(created_at, \'%Y年%m月%d日 %k時%i分%s秒\') AS created_at FROM messages WHERE board_id = ' + boardId;
+  let getMessagesQuery = 'SELECT M.message, ifnull(U.user_name, \'名無し\') AS user_name, DATE_FORMAT(M.created_at, \'%Y年%m月%d日 %k時%i分%s秒\') AS created_at FROM messages M LEFT OUTER JOIN users U ON M.user_id = U.user_id WHERE M.board_id = ' + boardId + ' ORDER BY M.created_at ASC';
   connection.query(getBoardQuery, function(err, board){
     connection.query(getMessagesQuery, function(err, messages){
       res.render('board', {
@@ -21,8 +21,9 @@ router.get('/:board_id', function(req, res, next){
 router.post('/:board_id', function(req, res, next){
   let message = req.body.message;
   let boardId = req.params.board_id;
+  let userId = req.session.user_id? req.session.user_id: 0;
   let createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
-  let query = 'INSERT INTO messages (message, board_id, created_at) VALUES ("' + message + '", ' + '"' + boardId + '", ' + '"' + createdAt + '")';
+  let query = 'INSERT INTO messages (message, board_id, user_id, created_at) VALUES ("' + message + '", ' + '"' + boardId + '", ' + '"' + userId + '", ' + '"' + createdAt + '")';
   connection.query(query, function(err, rows){
     res.redirect('/boards/' + boardId);
   });
