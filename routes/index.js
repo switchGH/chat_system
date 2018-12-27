92d0880fc86fa4cadda9239417c1c4d835584226
switch <shg18.sk1.hr10@gmail.com>
2018-12-27 15:23:58 +0900

    ボードにユーザーを紐づけ

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let query = 'SELECT *, DATE_FORMAT(created_at, \'%Y年%m月%d日 %k時%i分%s秒\') AS created_at FROM boards';
+  let query = 'SELECT B.board_id, B.user_id, B.title, ifnull(U.user_name, \'名無し\') AS user_name, DATE_FORMAT(B.created_at, \'%Y年%m月%d日 %k時%i分%s秒\') AS created_at FROM boards B LEFT OUTER JOIN users U ON B.user_id = U.user_id ORDER BY B.created_at DESC';
   connection.query(query, function(err, rows){
     console.log(rows);
     res.render('index', {
@@ -17,9 +17,10 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next){
   let title = req.body.title;
+  let userId = req.session.user_id? req.session.user_id: 0;
   let createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
   //console.log(createdAt);
-  let query = 'INSERT INTO boards (title, created_at) VALUES ("' + title + '", ' + '"' + createdAt + '")';
+  let query = 'INSERT INTO boards (user_id, title, created_at) VALUES ("' + userId + '", ' + '"' + title + '", ' + '"' + createdAt + '")';
   //console.log(query);
   connection.query(query, function(err, rows){
     //console.log(err);
