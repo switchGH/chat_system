23480f1fda40fdab9abdae98bd150440dc730450
switch <shg18.sk1.hr10@gmail.com>
2018-12-28 23:58:32 +0900

    画像のアップロードを実装

--- routes/boards.js
+++ routes/boards.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const moment = require('moment');
+const multer = require('multer');
 const router = express.Router();
 const connection = require('../model/mysqlConnection');
+const upload = multer({ dest: './public/images/uploads/' });
 
-router.get('/:board_id', function(req, res, next){
+router.get('/:board_id', function(req, res){
   let boardId = req.params.board_id;
   let getBoardQuery = 'SELECT * FROM boards WHERE board_id = ' + boardId;
   let getMessagesQuery = 'SELECT M.message, ifnull(U.user_name, \'名無し\') AS user_name, DATE_FORMAT(M.created_at, \'%Y年%m月%d日 %k時%i分%s秒\') AS created_at FROM messages M LEFT OUTER JOIN users U ON M.user_id = U.user_id WHERE M.board_id = ' + boardId + ' ORDER BY M.created_at ASC';
@@ -18,7 +20,8 @@ router.get('/:board_id', function(req, res, next){
   });
 });
 
-router.post('/:board_id', function(req, res, next){
+router.post('/:board_id', upload.single('image_file'), function(req, res){
+  console.log(req.file);
   let message = req.body.message;
   let boardId = req.params.board_id;
   let userId = req.session.user_id? req.session.user_id: 0;
